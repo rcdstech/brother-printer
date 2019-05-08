9e3a4ca0ffac01d7d3331131be1b8db09dca2e6c
parthshah <parthshah@cloud.upwork.com>
2019-05-08 21:36:48 +0530

    JSON file or json body to xml

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const xml = require('./server/xml');
+const port = 3000;
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.use('/xml', xml);
+app.listen(port, (err) => {
+	if(err) {
+		console.log(err)
+	}
+	console.log('server running on ', port)
+})
