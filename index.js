84c1efac9e58af4051df35bb7c5f3f863c8025de
vinod Chopra <rishabhchopra.dcs16@vnsgu.ac.in>
2019-05-14 00:45:11 +0530

    Added scantoemail button

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const xml = require('./server/xml');
 const port = 8081;
 const multer = require('multer');
+const fs = require('fs');
 const upload = multer({ dest: 'jsonFiles/' });
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -14,6 +15,14 @@ app.use('/xml', xml);
 
 app.post('/upload', upload.any(), (req, res) => {
 	res.send(req.files[0].filename);
+});
+
+app.post('/scanToEmail', (req, res) => {
+	console.log(req.body.data)
+	fs.writeFile(__dirname + '/server/scanToEmail.json', req.body.data, (err) => {
+		if (err) res.send(err);
+		res.send("Successfully Written to File.");
+	});
 });
 
 app.use('/', express.static(__dirname + '/view'))
